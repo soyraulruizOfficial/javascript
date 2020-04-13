dc002611cb3a33ec53c4c9616106c8202b66e05d
raulruiz94 <shaggyruliz16@gmail.com>
2020-04-13 11:06:57 -0500

    typescript

---
+++ POO/nose/src/index.ts
@@ -0,0 +1,6 @@
+console.log("Ya funciono perrro")
+
+const contenedor = document.getElementById("container")
+const input = document.createElement("input");
+input.classList.add("form-control")
+contenedor.appendChild(input)
